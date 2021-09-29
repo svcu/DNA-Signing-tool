20901eea950c6b42cb730c1e701cdc46c68b85ce
Sergio Valdés Valdés <60680494+svcu@users.noreply.github.com>
2021-09-29 16:36:32 -0400

    Add files via upload

---
+++ sign.js
@@ -0,0 +1,23 @@
+var EC = require('elliptic').ec;
+var ec = new EC('secp256k1');
+
+function sign(tx, privateKey){
+    let key = ec.keyFromPrivate(privateKey.toUpperCase())
+
+        
+
+    console.log(privateKey.toUpperCase())
+    console.log(tx.from.toUpperCase())
+
+    if(key.getPublic("hex").toUpperCase() !== tx.from.toUpperCase()){
+        return "Invalid Address"
+    }
+
+
+
+    const sig = key.sign(this.calculateHash(tx), "base64").toDER("hex");
+
+    return sig
+}
+
+module.exports = sign;
