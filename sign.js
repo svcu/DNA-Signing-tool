2211f71cc11f4b011c8cfad2bc5dc29b33d5b119
Sergio Valdés Valdés <60680494+svcu@users.noreply.github.com>
2021-09-29 16:38:40 -0400

    Update sign.js

--- sign.js
+++ sign.js
@@ -1,13 +1,13 @@
 var EC = require('elliptic').ec;
 var ec = new EC('secp256k1');
 
-function sign(tx, privateKey){
+function sign(tx, privateKey){ //tx=transaction JSON object
     let key = ec.keyFromPrivate(privateKey.toUpperCase())
 
         
-
+/*
     console.log(privateKey.toUpperCase())
-    console.log(tx.from.toUpperCase())
+    console.log(tx.from.toUpperCase())*/
 
     if(key.getPublic("hex").toUpperCase() !== tx.from.toUpperCase()){
         return "Invalid Address"
